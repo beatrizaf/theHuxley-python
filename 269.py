06e8e918f2617fb9099a71d747161123fe53ca8f
Beatriz Araújo <beatrizbjj1@gmail.com>
2022-04-11 19:37:08 -0300

    Create 269.py

---
+++ 269.py
@@ -0,0 +1,31 @@
+def MesCorrespondente(mes):
+     if mes == 1:
+         return("janeiro")
+     elif mes == 2:
+         return("fevereiro")
+     elif mes == 3:
+         return("marco")
+     elif mes == 4:
+         return("abril")
+     elif mes == 5:
+         return("maio")
+     elif mes == 6:
+         return("junho")
+     elif mes == 7:
+         return("julho")
+     elif mes == 8:
+         return("agosto")
+     elif mes == 9:
+         return("setembro")
+     elif mes == 10:
+         return("outubro")
+     elif mes == 11:
+         return("novembro")
+     elif mes == 12:
+         return("dezembro")
+     else:
+         return("invalido")
+         
+mes = int(input())
+print(MesCorrespondente(mes))
+     
