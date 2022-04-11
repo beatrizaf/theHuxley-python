47f509b7005e8f506c7277d0dddcdbff1b27c2a1
Beatriz Araújo <beatrizbjj1@gmail.com>
2022-04-11 11:55:32 -0300

    Create 1035.py

---
+++ 1035.py
@@ -0,0 +1,8 @@
+lado_menor = int(input())
+lado_maior = int(input())
+
+if (lado_menor >= 1) and (lado_menor <= 1000) and (lado_maior >= 1) and (lado_maior <= 1000):
+    area = lado_maior * lado_menor
+    print(area)
+
+    
