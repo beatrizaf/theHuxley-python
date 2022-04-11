9bae3d4b4912a2e70e3f30beae448c0351584b5d
Beatriz Araújo <beatrizbjj1@gmail.com>
2022-04-11 12:38:15 -0300

    Create 374.py

---
+++ 374.py
@@ -0,0 +1,12 @@
+salario = float(input())
+
+salariot = salario * 0.75
+
+if salariot < 2000:
+     print("ARGENTINA")
+     
+elif (salariot >= 2000) and (salariot <= 3000):
+     print("ESPANHA")
+     
+else:
+     print("ALEMANHA")
